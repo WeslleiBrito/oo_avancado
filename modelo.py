d39e3c1030a5e0ca3f34323208a169c30a4f05d9
WeslleiBrito <wesllei.wbs@gmail.com>
2021-11-24 16:15:22 -0300

    Criando uma classe iteravel

--- modelo.py
+++ modelo.py
@@ -26,6 +26,9 @@ class Filmes(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def __str__(self):
+        return f'{self._nome} - {self.duracao} min - {self._likes}'
+
 
 class Serie(Programa):
 
@@ -33,16 +36,59 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.temporadas} temporadas - {self._likes}'
+
+
+class Playlist:
+    def __init__(self, nome, programa):
+        self._nome = nome
+        self._programa = programa
+
+    def __getitem__(self, item):
+        return self._programa[item]
+
+    @property
+    def listagem(self):
+        return self._programa
+
+    @property
+    def tamanho(self):
+        return len(self._programa)
+
 
 vingadores = Filmes('vingadores ultimato', 2018, 120)
-for c in range(0, 5):
-    vingadores.recebe_likes()
-print(f'Descrição do filme:\nNome: {vingadores.nome}\nLançamento: {vingadores.ano}\nDuração: {vingadores.duracao}'
-      f'\nLikes: {vingadores.likes}')
+missao_resgate = Filmes('missão resgate', 2021, 109)
+resident_evil = Filmes('bem vindo a raccoon city', 2021, 107)
+
+vingadores.recebe_likes()
+vingadores.recebe_likes()
+vingadores.recebe_likes()
+vingadores.recebe_likes()
+missao_resgate.recebe_likes()
+missao_resgate.recebe_likes()
+resident_evil.recebe_likes()
+resident_evil.recebe_likes()
+resident_evil.recebe_likes()
 
 peaky_blinders = Serie('peaky blinders', 2014, 5)
-for c in range(0, 3):
-    peaky_blinders.recebe_likes()
-print(f'\nDescrição da serie:\nNome: {peaky_blinders.nome}\nLançamento: {peaky_blinders.ano}\nDuração: '
-      f'{peaky_blinders.temporadas}\nLikes: {peaky_blinders.likes}')
+twot = Serie('The Wheel Of Time', 2021, 1)
+tdw = Serie('the walking dead', 2010, 10)
 
+peaky_blinders.recebe_likes()
+peaky_blinders.recebe_likes()
+tdw.recebe_likes()
+tdw.recebe_likes()
+tdw.recebe_likes()
+tdw.recebe_likes()
+tdw.recebe_likes()
+twot.recebe_likes()
+twot.recebe_likes()
+twot.recebe_likes()
+
+lista = [vingadores, missao_resgate, resident_evil, peaky_blinders, tdw, twot]
+minha_playlist = Playlist('Minha Play', lista)
+
+print(minha_playlist[0])
+for programas in minha_playlist:
+    print(programas)
